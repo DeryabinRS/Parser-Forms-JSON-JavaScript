c66df9e81cee29f75916333c89b0182d3d00efd5
Deryabin Roman <deryabinrs@gmail.com>
2021-10-10 22:56:36 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+class JsonParser{
+    constructor(url){
+        this.url = url
+        this.json = this.getJson()
+    }
+    async getJson() {
+        try{
+            const response = await fetch(this.url)
+                .then(response => response.json())
+            console.log(response)
+            return response
+        }catch(err){
+            console.log(err)
+        }
+    }
+    async getName() {
+
+    }
+}
