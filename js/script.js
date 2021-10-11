52622e2c7ccf5f35ddf7756689955f05806a39bf
DeryabinRS <deryabinrs@gmail.com>
2021-10-11 15:30:45 +0700

    create nav, add form

--- js/script.js
+++ js/script.js
@@ -1,33 +1,41 @@
-class JsonParser{
-    constructor(url){
-        this.url = url
-        this.json = this.getJson()
-    }
-    async getJson() {
-        try{
-            const response = await fetch(this.url)
-                .then(response => response.json())
-            console.log(response)
-            return response
-        }catch(err){
-            console.log(err)
-        }
+window.addEventListener('DOMContentLoaded', () => {
+    const app = document.querySelector('#app');
+    const navLinks =  document.querySelectorAll('.nav-link');
+
+    navLinks.forEach( item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+        });
+    });
+
+    console.log(navLinks)
+    let dataJson = {};
+
+    async function assemblyForm(fileName){
+        //get data from JSON
+        const jsp = new JsonParser(`/json/${fileName}`);
+        dataJson = await jsp.json
+
+        //create card
+        const card = document.createElement('div');
+        card.classList.add('card');
+        app.append(card);
+        
+        //create title card
+        const card_title = document.createElement('div');
+        card_title.classList.add('card-header');
+        card_title.textContent = dataJson.name
+        card.append(card_title)
+
+        //create body card
+        const card_body = document.createElement('div');
+        card_body.classList.add('card-body');
+        card.append(card_body);
+
+        //Create form
+        const form = document.createElement('form');
+        card_body.appendChild(form)
     }
 
-    getName() {
-        return this.json.name;
-    }
-
-    getFields(){
-        return this.json.fields;
-    }
-
-    getButtons(){
-        return this.json.buttons;
-    }
-
-    getReferences(){
-        return this.json.buttons;
-    }
-    
-}
+    assemblyForm('addpost.js');
+})
---
+++ store/JsonParser.js
@@ -0,0 +1,33 @@
+class JsonParser{
+    constructor(url){
+        this.url = url
+        this.json = this.getJson()
+    }
+    async getJson() {
+        try{
+            const response = await fetch(this.url)
+                .then(response => response.json())
+            //console.log(response)
+            return response
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    getName() {
+        return this.json.name;
+    }
+
+    getFields(){
+        return this.json.fields;
+    }
+
+    getButtons(){
+        return this.json.buttons;
+    }
+
+    getReferences(){
+        return this.json.buttons;
+    }
+    
+}
