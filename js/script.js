44186035cfde877cfd707305a4b7cef19e6109fc
DeryabinRS <deryabinrs@gmail.com>
2021-10-14 16:34:05 +0700

    fix color

--- js/script.js
+++ js/script.js
@@ -60,12 +60,13 @@ window.addEventListener('DOMContentLoaded', () => {
             wrapper.classList.add('mb-3');
             block.appendChild(wrapper);
 
-            id = field?.label?.toLowerCase().replace(/\s/g, '_',)
+            // id = field?.label?.toLowerCase().replace(/\s/g, '_',)
+            id = `field_${index}`
             if(field.label){
                 const mask = field?.input?.mask && field?.input?.mask
                 assemblyLabel(wrapper, field.label, id, mask);
             }
-            assemblyInput(wrapper, field, id)
+            assemblyInput(wrapper, field, id, index)
 
         });
     }
@@ -80,13 +81,12 @@ window.addEventListener('DOMContentLoaded', () => {
         wrapper.appendChild(label);
     }
 
-    function assemblyInput(wrapper, field, id = ''){
+    function assemblyInput(wrapper, field, id = '', idEl = 0){
 
         let input;
         switch(field.input.type){
             case 'checkbox':
                 input = document.createElement('input'); 
-                console.log(132)
                 input.classList.add('form-check-input');
                 break;
             case 'textarea':
@@ -94,25 +94,14 @@ window.addEventListener('DOMContentLoaded', () => {
                 input.classList.add('form-control');
                 break;
             case 'technology':
-                input = document.createElement('select');
-                if(field.input.multiple){
-                    input.setAttribute('multiple', true);
-                }
-                input.classList.add('form-select');
-                field.input.technologies.forEach((tehnology, index) => {
-                    const option = document.createElement('option');
-                    option.setAttribute('value', index);
-                    option.innerText = tehnology
-                    input.appendChild(option)
-                })
+                input = assemblySelect(field.input, field.input.technologies, wrapper, idEl, 'Select items')
+                break;
+            case 'color':
+                input = assemblyColor(field.input, wrapper)
                 break;
             default:
                 input = document.createElement('input');
                 input.classList.add('form-control');
-                if(field.input.type === 'color'){
-                    input.classList.add('form-control-color')
-                }
-                
         }
         
         input.setAttribute('id', id);
@@ -139,6 +128,93 @@ window.addEventListener('DOMContentLoaded', () => {
         
 
         wrapper.appendChild(input)
+    }
+
+    function assemblyColor(fieldSetting, wrapper){
+        const colorbox = document.createElement('div');
+        wrapper.appendChild(colorbox)
+        if(Array.isArray(fieldSetting.colors)){
+            fieldSetting.colors.forEach((item, index) => {
+                const color = `<div style="background:${item}; width:80px; height:40px"></div>`
+                assemblyCheckbox(colorbox, color, `color_${index}`, 'radio', 'color1');
+            })
+        }
+        return colorbox;
+    }
+
+    function assemblyCheckbox (el, text, id = '', type = 'checkbox', name = '') {
+        const selectedBox = document.createElement('div');
+        selectedBox.classList.add('form-check')
+        el.appendChild(selectedBox);
+
+        const inputSelectedBox = document.createElement('input');
+        inputSelectedBox.classList.add('form-check-input')
+        inputSelectedBox.setAttribute('type', type);
+        if(type = 'radio') inputSelectedBox.setAttribute('name', name)
+        inputSelectedBox.setAttribute('id', id)
+        selectedBox.appendChild(inputSelectedBox);
+
+        const labelSelectedBox = document.createElement('label');
+        labelSelectedBox.classList.add('form-check-label')
+        labelSelectedBox.setAttribute('for', id)
+        labelSelectedBox.innerHTML = text
+        selectedBox.appendChild(labelSelectedBox);
+        return selectedBox;
+    }
+
+    function assemblySelect(fieldSetting, valuesArray = [], wrapper, idEl = 0, firstOptionName = ''){
+        const idSelect = `select_${idEl}`
+        let input;
+        const checkboxes = document.createElement('div');
+
+        if(fieldSetting.multiple){
+            input = document.createElement('div');
+            checkboxes.classList.add('checkboxes' ,'mb-3');
+            wrapper.after(checkboxes);
+        }else{
+            input = document.createElement('select');
+        }
+
+        input.setAttribute('id', idSelect);
+
+        input.classList.add('form-select');
+
+
+        if(firstOptionName){
+            const firstOption = document.createElement('option');
+            firstOption.innerHTML = firstOptionName;
+            input.appendChild(firstOption)
+        }
+
+        valuesArray.forEach((item, index) => {
+            const idOption = `${idSelect}_${index}`
+            const option = document.createElement('option');
+            option.setAttribute('value', index);
+            if(fieldSetting.multiple){
+                assemblyCheckbox(checkboxes, item, idOption)
+            }else{
+                console.log(item)
+                option.innerHTML = item
+                input.appendChild(option)
+            }
+        })
+
+
+        let expanded = false;
+
+        if(fieldSetting.multiple){
+            input.addEventListener('click', (e) => {
+                if (!expanded) {
+                    checkboxes.style.display = "block";
+                    expanded = true;
+                  } else {
+                    checkboxes.style.display = "none";
+                    expanded = false;
+                  }
+            })
+        }
+
+        return input;
     }
 
     function assemblyRef(wrapper, field){
